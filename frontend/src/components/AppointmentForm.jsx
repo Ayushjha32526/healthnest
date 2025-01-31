5838b39d65d7dbdac017f30ac2ff8d5501d23dcd
Ayushjha32526 <ayushjha32526@gmail.com>
2025-02-01 01:22:09 +0530

    frontend deployed

--- frontend/src/components/AppointmentForm.jsx
+++ frontend/src/components/AppointmentForm.jsx
@@ -25,7 +25,7 @@ const AppointmentForm = () => {
     useEffect(() => {
         const fetchDoctors = async () => {
             const { data } = await axios.get(
-                "http://localhost:4000/api/v1/user/doctors",
+                "https://healthnest.onrender.com/api/v1/user/doctors",
                 { withCredentials: true }
             );
             setDoctors(data.doctors);
@@ -38,7 +38,7 @@ const AppointmentForm = () => {
         try {
             const hasVisitedBool = Boolean(hasVisited);
             const { data } = await axios.post(
-                "http://localhost:4000/api/v1/appointment/post",
+                "https://healthnest.onrender.com/api/v1/appointment/post",
                 {
                     firstName,
                     lastName,
