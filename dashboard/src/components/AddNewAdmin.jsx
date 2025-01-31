2e43b427ac6059f0cae8e830ca9e953251ebe78c
Ayushjha32526 <ayushjha32526@gmail.com>
2025-02-01 01:34:08 +0530

    frontend deployed

--- dashboard/src/components/AddNewAdmin.jsx
+++ dashboard/src/components/AddNewAdmin.jsx
@@ -23,7 +23,7 @@ const AddNewAdmin = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/admin/addnew",
+          "https://healthnest.onrender.com/api/v1/user/admin/addnew",
           { firstName, lastName, email, phone,  dob, gender, password },
           {
             withCredentials: true,
--- dashboard/src/components/AddNewDoctor.jsx
+++ dashboard/src/components/AddNewDoctor.jsx
@@ -50,7 +50,7 @@ const AddNewDoctor = () => {
       formData.append("doctorDepartment", doctorDepartment);
       formData.append("docAvatar", docAvatar);
       await axios
-        .post("http://localhost:4000/api/v1/user/doctor/addnew", formData, {
+        .post("https://healthnest.onrender.com/api/v1/user/doctor/addnew", formData, {
           withCredentials: true,
           headers: { "Content-Type": "multipart/form-data" },
         })
